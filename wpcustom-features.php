5213b51dad40a501d8126667c19044415cb6ca9b
Rajan Gupta <rajangpt74@gmail.com>
2023-02-25 09:49:01 +0530

    First commit

---
+++ wpcustom-features.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name:       WPcustom Features
+ * Plugin URI:        https://fakerpress.com
+ * Description:       WPcustom Features is a clean way to generate fake data to your WordPress installation, great for developers who need testing
+ * Version:           1.0
+ * Author:            Rajan Gupta
+ * Author URI:        https://github.com/rajan1308
+ * Text Domain:       auilafeatures
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/bordoni/fakerpress
+ */
